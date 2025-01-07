b770eb904b37942b92aea3ca73505e022dc18778
FaseehHayat21 <faseehhayat999@gmail.com>
2025-01-07 21:44:00 +0500

    updare

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post('/alerts/refill', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;  // Use Vercel's PORT environment variable or default to 5001
+const PORT =  3000;  // Use Vercel's PORT environment variable or default to 5001
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
